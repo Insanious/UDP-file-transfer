25e2180d13da5007f2408daf13b78f117a5e607c
Anton Lindstrand <anton.lindstrand0@gmail.com>
2019-11-05 18:57:00 +0100

    Refactoring and optimizing

--- UDPclient.py
+++ UDPclient.py
@@ -18,10 +18,8 @@ import itertools
 
 META_RESEND_TIME = 0.5
 PACKET_SIZE = 1024
-CHUNK_SIZE = 5000
-WAIT_TIME_BETWEEN_CATCHES = 0.5
+CHUNK_SIZE = 2048
 MAX_TIME_BETWEEN_PACKETS = 0.05
-MAX_TRIES = 10
 START_SEQUENCE_NUMBER = 100000
 SERVER_PORT = 80
 SERVER_DONE = "SERVER_DONE"
@@ -35,7 +33,6 @@ def format_lost_packets(lost_packets):
 	for packet in lost_packets:
 		splitted = packet.split(";")
 		splitted = splitted[:-1]
-
 		for sequence_nr in splitted:
 			sequence_nrs.append(int(sequence_nr))
 
@@ -70,15 +67,13 @@ def create_file_meta_packet(file_name):
 	sequence_nr = START_SEQUENCE_NUMBER
 	size = int(size + (size / PACKET_SIZE) * 7)
 	nr_of_data_packets = int(math.ceil(size / PACKET_SIZE))
-	nr_of_chunks = int(math.ceil(nr_of_data_packets / CHUNK_SIZE))
+	nr_of_chunks = int(math.ceil(nr_of_data_packets / (CHUNK_SIZE - 1))) # -1 to account for the meta-chunk packet
 
-	while True:
-		if int(math.ceil((nr_of_data_packets + nr_of_chunks) / CHUNK_SIZE)) > nr_of_chunks:
-			nr_of_chunks = int(math.ceil((nr_of_data_packets) + nr_of_chunks / CHUNK_SIZE))
-		else:
-			break
-
-	meta = str(sequence_nr) + ";" + str(size) + ";" + file_name + ";" + str(nr_of_data_packets) + ";" + str(nr_of_chunks)
+	meta = str(sequence_nr) + ";"
+	meta += str(size) + ";"
+	meta += file_name + ";"
+	meta += str(nr_of_data_packets) + ";"
+	meta += str(nr_of_chunks)
 	return meta
 
 
@@ -92,24 +87,22 @@ def create_chunk_meta_packet(sequence_nr, remaining_packets):
 def file_to_chunks(file_name):
 	chunks = []
 	packet_counter = 0
-	chunk_nr = -1
+	chunk_nr = -1 # because it gets incremented at first iteration
 	meta_packet = create_file_meta_packet(file_name)
-	#nr_of_chunks = meta_packet.split(";")[-1]
 	remaining_packets = int(meta_packet.split(";")[-2])
 
 	sequence_nr = START_SEQUENCE_NUMBER
 
 	with open(file_name, "r") as file:
 		while True:
-			sequence_nr += 1
-
 			if packet_counter % CHUNK_SIZE == 0:
 				chunks.append([])
 				chunk_nr += 1
-				chunks[chunk_nr].append(create_chunk_meta_packet(sequence_nr, remaining_packets))
 				sequence_nr += 1
 				packet_counter += 1
+				chunks[chunk_nr].append(create_chunk_meta_packet(sequence_nr, remaining_packets + 1)) # +1 to account for the chunk-meta packet
 
+			sequence_nr += 1
 			data = file.read(PACKET_SIZE - len(str(sequence_nr)) - 1) # -1 to account for the delimeter
 			if not data:
 				break
@@ -122,125 +115,63 @@ def file_to_chunks(file_name):
 	return meta_packet, chunks
 
 
-def file_to_packets(file_name):
-	packets = []
-	sequence_nr = START_SEQUENCE_NUMBER
-
-	with open(file_name, "r") as file: # create packets packets with PACKET_SIZE from file_name
-		while True:
-			sequence_nr += 1
-			data = file.read(PACKET_SIZE - len(str(sequence_nr)) - 1) # -1 to account for the ";"
-			if not data:
-				break
-
-			data = str(sequence_nr) + ";" + data
-			packets.append(data)
-
-	return packets
-
-
-def extract_arguments():
-	if len(sys.argv) != 3: # sys.argv=[file_name, server_ip]
-		print("invalid arguments")
-		sys.exit(0)
-
-	file_name = sys.argv[1]
-	server_ip = sys.argv[2]
-
-	return file_name, server_ip
-
-
 def send_all_chunks(client_socket, server_ip, meta_packet, chunks):
-	print("send_all_chunks()")
 	total_lost_packets = 0
 
 	packets = []
 	packets.append(meta_packet)
 	for chunk in chunks: # get packets to 1d array
 		for packet in chunk:
+			#print(packet.split(";")[0])
 			packets.append(packet)
 
 	while True:
 		if send_meta_and_listen(client_socket, server_ip, meta_packet): # send file meta
 			break
-	print("meta success")
+	print("-META SUCCESS")
 
 	for chunk in chunks: # send all chunks
-		print("sending chunk")
 		nr_of_lost_packets_in_chunk = 0
-		nr_of_packets = len(chunk) - 1 # -1 to remove chunk meta
+		nr_of_packets = len(chunk) # -1 to remove chunk meta
 
 		while True: # send chunk meta
 			if send_meta_and_listen(client_socket, server_ip, chunk[0]):
+				chunk_nr = (int(chunk[0].split(";")[0]) - START_SEQUENCE_NUMBER + 1) / CHUNK_SIZE
+				print("-SENDING CHUNK " + str(int(chunk_nr)))
 				break
-		#print("chunk meta success, " + chunk[-1])
 
-		for i in range(1, nr_of_packets): # send all packets
+		for i in range(1, nr_of_packets): # send all packets, start at 1 because the first packet has already been sent (meta-chunk)
 			client_socket.sendto(chunk[i].encode(), (server_ip, SERVER_PORT))
-		print("dis how many: " + str(len(chunk)))
-		print(str(chunk[1].split(";")[0]) + " first boi")
-		print(str(chunk[-1].split(";")[0]) + " last boi")
 
-		while True: # resend while there are any lost packets
-			nr_of_lost_packets = listen_and_send_lost_packets(client_socket, server_ip, packets)
-			if nr_of_lost_packets == 0:
-				break
-
+		nr_of_lost_packets = listen_and_send_lost_packets(client_socket, server_ip, packets)
+		while nr_of_lost_packets != 0: # resend while there are any lost packets
 			nr_of_lost_packets_in_chunk += nr_of_lost_packets
+			nr_of_lost_packets = listen_and_send_lost_packets(client_socket, server_ip, packets)
 
 		total_lost_packets += nr_of_lost_packets_in_chunk
 
-	print("All chunks sent with a total of " + str(total_lost_packets) + " lost packets!")
+	print("All chunks sent with a total of " + str(total_lost_packets) + " lost packets out of " + str(len(packets)) + "!")
 
 
 def listen_and_send_lost_packets(client_socket, server_ip, packets):
 	lost_packets = []
 	lost_sequence_nrs = listen_for_lost_packets(client_socket)
+	nr_of_lost_packets = len(lost_sequence_nrs)
 
-	if len(lost_sequence_nrs) == 0:
+	if nr_of_lost_packets == 0: # happens when server sends SERVER_DONE
 		return 0
 
 	for sequence_nr in lost_sequence_nrs:
-		lost_packets.append(packets[int(sequence_nr)-START_SEQUENCE_NUMBER])
+		lost_packets.append(packets[int(sequence_nr)-START_SEQUENCE_NUMBER]) # packet_index = seq - start_seq
 
-	#print(str(lost_sequence_nrs).strip('[]'))
-
-	#print("-" + str(len(lost_sequence_nrs)) + " lost packets, resending")
 	send_all(client_socket, server_ip, lost_packets)
 
-	return len(lost_sequence_nrs)
+	return nr_of_lost_packets
 
 
 def send_all(client_socket, server_ip, packets):
 	for packet in packets:
 		client_socket.sendto(packet.encode(),(server_ip, SERVER_PORT))
-
-
-
-def send_all_packets(client_socket, server_ip, packets):
-	nr_of_lost_packets = 0
-
-	send_meta_and_listen(client_socket, server_ip, packets[0])
-
-	for i in range(1, len(packets)): # Send all data packets, start at 1 because meta has already been sent
-		client_socket.sendto(packets[i].encode(),(server_ip, SERVER_PORT))
-
-	while True: # Listen for any packets lost
-		lost_packets = []
-		lost_sequence_nrs = []
-
-		lost_sequence_nrs = listen_for_lost_packets(client_socket)
-
-		nr_of_lost_packets += len(lost_sequence_nrs)
-		if len(lost_sequence_nrs) == 0:
-			print(str(len(packets)) + " " + str(nr_of_lost_packets))
-			break
-
-		for sequence_nr in lost_sequence_nrs:
-			lost_packets.append(packets[int(sequence_nr)-START_SEQUENCE_NUMBER])
-
-		for packet in lost_packets:
-			client_socket.sendto(packet.encode(),(server_ip, SERVER_PORT)) # Send packet
 
 
 def handle_socket_errors(error):
@@ -258,7 +189,7 @@ def listen_for_lost_packets(client_socket):
 
 	while True:
 		try:
-			if incoming_packets and time.clock() - last_packet_time > MAX_TIME_BETWEEN_PACKETS: # too long time between packets, calculate and send missing packets back
+			if incoming_packets and time.clock() - last_packet_time > MAX_TIME_BETWEEN_PACKETS: # too long time between packets
 				return format_lost_packets(lost_sequence_nrs)
 
 			packet, server_address = client_socket.recvfrom(PACKET_SIZE)
@@ -283,17 +214,24 @@ def listen_for_lost_packets(client_socket):
 def create_socket():
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	client_socket.setblocking(False)
-
 	return client_socket
+
+
+def extract_arguments():
+	if len(sys.argv) != 3: # sys.argv=[python_file(UDPclient.py), file_name, server_ip]
+		print("invalid arguments")
+		sys.exit(0)
+
+	file_name = sys.argv[1]
+	server_ip = sys.argv[2]
+	return file_name, server_ip
 
 
 def main():
 	file_name, server_ip = extract_arguments()
 	client_socket = create_socket()
-	#packets = file_to_packets(file_name)
 	meta_packet, chunks = file_to_chunks(file_name)
 
-	#send_all_packets(client_socket, server_ip, packets)
 	send_all_chunks(client_socket, server_ip, meta_packet, chunks)
 
 	client_socket.close()
